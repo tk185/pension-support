d9eb33d4d848d20e0d24c2ec5b63375ee0b32483
tk185 <tomasz.kolecki@engineering.digital.dwp.gov.uk>
2021-03-30 12:53:41 +0100

    update

--- app/routes.js
+++ app/routes.js
@@ -85,4 +85,25 @@ router.post('/missingcope3_2/mc', function (req, res) {
   }
 })
 
+router.post('/missingcope2_3/mc', function (req, res) {
+  const cope = req.session.data['customer']
+
+  if (cope === '3') {
+    res.redirect('/missingcope2_3/missing')
+  } else {
+    res.redirect('/missingcope2_3/missing2')
+  }
+})
+
+router.post('/missingcope3_3/mc', function (req, res) {
+  const cope = req.session.data['customer']
+
+  if (cope === '3') {
+    res.redirect('/missingcope3_3/missing')
+  } else {
+    res.redirect('/missingcope3_3/missing2')
+  }
+})
+
+
 module.exports = router
