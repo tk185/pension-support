9003aa6c22b11427c271fd283b04a386ea3a59a1
tk185 <tomasz.kolecki@engineering.digital.dwp.gov.uk>
2021-06-30 22:37:59 +0100

    update

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'SFCYSP',
+  serviceName: 'Support for Check your State Pension forecast',
 
   // Default port that prototype runs on
   port: '3000',
