2ec0cc193faae824e40cfcb01982ead28e80e292
tk185 <tomasz.kolecki@engineering.digital.dwp.gov.uk>
2022-06-28 09:59:13 +0100

    update

--- app/routes.js
+++ app/routes.js
@@ -291,17 +291,7 @@ router.post('/RoleC0_2/legrep', function (req, res) {
   }
 })
 
-router.post('/MQP_letter0_1/third_party', function (req, res) {
-  const type = req.session.data['type']
-
-  if (type === 'main') {
-    res.redirect('/MQP_letter0_1/name2')
-  } else {
-    res.redirect('/MQP_letter0_1/preview')
-  }
-})
-
-router.post('/MQP_letter0_1/third_party', function (req, res) {
+router.post('/MQP_Letter0_1/third_party', function (req, res) {
   const type = req.session.data['type']
 
   if (type === 'main') {
@@ -310,6 +300,7 @@ router.post('/MQP_letter0_1/third_party', function (req, res) {
     res.redirect('/MQP_Letter0_1/preview')
   }
 })
+
 
 
 module.exports = router
